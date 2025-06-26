e2f084148ef634dee31c9e1b8a522c3b8a5cfa17
Manthan <manthanjasoliya84014@gmail.com>
2025-06-26 14:15:41 +0300

    Add files via upload

---
+++ week8/homepage/javascript.js
@@ -0,0 +1,8 @@
+// Add interactivity to the button
+document.addEventListener("DOMContentLoaded", () => {
+    const alertButton = document.getElementById("alertButton");
+
+    alertButton.addEventListener("click", () => {
+        alert("Hello, welcome to my homepage!");
+    });
+});
